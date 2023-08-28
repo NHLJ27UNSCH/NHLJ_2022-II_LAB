e6d533eeff8f2b316a0d7af5d9c43dc78f58647d
NHLJ27UNSCH <143233910+NHLJ27UNSCH@users.noreply.github.com>
2023-08-28 15:49:26 -0500

    Update LAB01 EJ1.py

--- LAB01 EJ1.py
+++ LAB01 EJ1.py
@@ -1,12 +1,23 @@
 #Determinar si un número es primo
-num=int(input("numero a determinar: "))
-
-if num==1:
-    print('1 no es primo')
-elif num>1:
-    for i in range(2, num):
-        if (num%i)==0: #se halla el resto de dividir el numero desde 2 hasta el mismo numero para encontrar factores
-            print('no es primo')
-            break
+def num_primo():
+    try:
+        num=int(input("numero a determinar: "))
+        if num==1:
+            print('1 no es numero primo')
+        elif num==0:
+            print('0 no es un numero primo')
+        
+        elif num>1:
+            for i in range(2, num):
+                if (num%i)==0: #se halla el resto de dividir el numero desde 2 hasta el mismo numero para encontrar factores
+                    print('no es primo')
+                    break
+                else:
+                    print('es primo!')
         else:
-            print('es primo!')
+            print('error!')
+    except ValueError:
+        print('Error de valor!')
+        num_primo()
+
+num_primo()
